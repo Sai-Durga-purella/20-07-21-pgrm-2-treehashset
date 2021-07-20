c1e101ea2042c1a9217218773350e46dd8e6cb57
Sai-Durga-purella <71429521+Sai-Durga-purella@users.noreply.github.com>
2021-07-20 09:59:14 +0530

    Add files via upload

---
+++ maps1.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class map1
+
+{
+  
+public static void main(String args[])
+  
+{
+    
+HashMap<Integer,String>x=new HashMap<Integer,String>();
+    
+x.put(5,"john");
+    
+x.put(1,"sam");
+    
+x.put(7,"kim");
+   
+TreeMap<Integer,String>t=new TreeMap<Integer,String>();
+    
+ System.out.println(x);
+  
+}
+
+}
+
+
+
+output:
+         {1=sam, 5=john, 7=kim}
